c8bc5abf1a7fa7ac75fa3332591764ee577d9041
opposition pantophobia <162717999+Drop56796@users.noreply.github.com>
2024-04-19 22:27:50 +0800

    Update Noah Dark.lua

--- Noah Dark.lua
+++ Noah Dark.lua
@@ -7,6 +7,77 @@ wait(5)
 require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("The Ambush and Rush is Nightmare",true)
 wait(5)
 
+-- Custom Door Sounds
+loadstring(game:HttpGet("https://pastebin.com/raw/iAhTGdBh"))()
+loadstring(game:HttpGet("https://pastebin.com/raw/6brG9uma"))()
+
+-- Overseer Eyes
+coroutine.wrap(function()
+    while true do
+        wait(math.random(3,100))
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+wait(0.5)
+local enableDamage = true
+ 
+local currentLoadedRoom=workspace.CurrentRooms[game:GetService("ReplicatedStorage").GameData.LatestRoom.Value]
+local eyes=game:GetObjects("rbxassetid://12285389022")[1]
+ 
+if eyes then end
+game.Workspace.CurrentRooms.ChildAdded:Connect(function()
+    game.Workspace:FindFirstChild("Core"):Destroy()
+    enableDamage = true
+end)
+local num=math.floor(#currentLoadedRoom.Nodes:GetChildren()/2)
+eyes.CFrame=(
+	num==0 and currentLoadedRoom.Base or currentLoadedRoom.Nodes[num]
+).CFrame+Vector3.new(0,3,0)
+ 
+eyes.Parent=workspace
+ 
+local hum=game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+while true and enableDamage do
+if not game.Workspace:FindFirstChild("Core") then break end
+	local _,found=workspace.CurrentCamera:WorldToScreenPoint(eyes.Position)
+	if not found then
+		hum.Health-=10
+		eyes.Attack:Play()
+		if hum.Health<=0 then
+			game:GetService("ReplicatedStorage").GameStats["Player_".. game.Players.LocalPlayer.Name].Total.DeathCause.Value = "Overseer Eyes"
+			firesignal(game.ReplicatedStorage.EntityInfo.DeathHint.OnClientEvent, {"You've been consumed by the Overseer Eyes.", "They want you to obey and look at them.", "If you dont look at them while they look at you, they will damage."}, "Blue")
+		end
+	end
+	task.wait(.25)
+end
+    end
+end)()
+
+-- Claim
+coroutine.wrap(function()
+    while true do
+        wait(math.random(100,550))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        loadstring(game:HttpGet("https://pastebin.com/raw/d3R357Rk"))()
+    end
+end)()
+
+-- Zombie
+coroutine.wrap(function()
+    while true do
+        wait(math.random(1,1250))
+        loadstring(game:HttpGet("https://pastebin.com/raw/DG9MvSPZ"))()
+    end
+end)()
+
+-- the intruder
+coroutine.wrap(function()
+    while true do
+        wait(math.random(20,350))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(2)
+loadstring(game:HttpGet("https://pastebin.com/raw/8p9VKFF0"))()
+    end
+end)()
+
 -- rush ambush nightmare
 
 coroutine.wrap(function()
