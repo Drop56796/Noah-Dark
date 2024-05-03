2e8fe627055ca954f4e04f29998096f9b6f058ca
opposition pantophobia <162717999+Drop56796@users.noreply.github.com>
2024-05-03 09:49:05 +0800

    Update Darknightmare Mode V6.lua

--- Darknightmare Mode V6.lua
+++ Darknightmare Mode V6.lua
@@ -77,7 +77,7 @@ end)()
 -- Super A60
 coroutine.wrap(function()
     while true do
-        wait(math.random(1800,1900))
+        wait(math.random(1000,1025))
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/a60/main/60.lua"))()
     end
@@ -86,7 +86,7 @@ end)()
 -- detph
 coroutine.wrap(function()
     while true do
-        wait(math.random(1200,1400))
+        wait(math.random(890,900))
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/detph-spawner/main/detph.lua"))()
     end
@@ -95,7 +95,7 @@ end)()
 -- Crazy Hunger
 coroutine.wrap(function()
     while true do
-        wait(math.random(1100,1700))
+        wait(math.random(900,915))
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Crazy-Hunger/main/Hunger.lua"))()
     end
@@ -104,7 +104,7 @@ end)()
 -- terrorist
 coroutine.wrap(function()
     while true do
-        wait(math.random(1400,2000))
+        wait(math.random(999,1000))
         game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Drop56796/Spawn-terrorist/main/Spawn.lua"))()
     end
